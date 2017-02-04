3a040d960091fb4fb75f5528398d83cbb049cf03
abinshoby <abinshoby@gmail.com>
2017-02-04 16:40:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,7 +15,7 @@ var counter=0;
 var button=document.getElementById("counter");
 button.onclick=function(){
     counter=counter+1;
-    var span=document.getElementById("count");
-    span.innerHTML=counter.toString();
+    var spam=document.getElementById("count");
+    spam.innerHTML=counter.toString();
     
 };
