0b8ec129385f3a935f58039006cf381a9f747d10
abinshoby <abinshoby@gmail.com>
2017-02-04 15:47:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById("maintext");
-element.innerHTML="new value"
+element.innerHTML="new value";
+var img=document.getElementById("move");
+img.onclick=function(){
+    img.style.marginleft="100px";
+}
