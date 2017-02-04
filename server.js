0816f8a7c897f93137ae9c5e17a1e03386658107
abinshoby <abinshoby@gmail.com>
2017-02-04 16:21:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,7 +57,14 @@ app.get('/', function (req, res) {
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+  
 });
+var counter=0;
+app.get('/counter',function(req,res){
+   counter=counter+1;
+   res.send(counter.tostring());
+});
+
 app.get('/:articlename',function(req,res){
     var articlename=req.params.articlename;
     res.send(createtemplete(articles[articlename]));});
@@ -67,11 +74,6 @@ app.get('/ui/main.js', function (req, res) {
     
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-var counter=0;
-app.get('/counter',function(req,res){
-   counter=counter+1;
-   res.send(counter.tostring());
 });
 
 
